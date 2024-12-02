092509d7b4dd804ffb2dea8b68cf4871cc8509c7
DiogoJRC <diogoj.r.c@hotmail.com>
2024-12-02 01:33:59 +0000

    Adição da página inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <><h1>Criei isso para dizer que você é uma pessoa muito especial🧡</h1>
+    <small>Tentei pensar em algo mais criativo, mas não consegui 🥲</small></>;
+}
+
+export default Home;
